f4b2d425b2548230240975ad1e2fbadf27e7e33d
zzzhim <3218566389@qq.com>
2022-03-09 00:05:24 +0800

    ts转图片测试

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 import Koa from "koa"
 import path from "path"
-// const jwt = require("koa-jwt")
-import cors from "@koa/cors"
-import logger from "koa-logger"
-import render from "koa-ejs"
-import koaBody from "koa-body"
-import session from "koa-session"
-import koaStatic from 'koa-static'
-import { SECRET_KEY, SESSION_KEY } from "./src/config"
-import { router } from "./src/router"
+// // const jwt = require("koa-jwt")
+// import cors from "@koa/cors"
+// import logger from "koa-logger"
+// import render from "koa-ejs"
+// import koaBody from "koa-body"
+// import session from "koa-session"
+// import koaStatic from 'koa-static'
+// import { SECRET_KEY, SESSION_KEY } from "./src/config"
+// import { router } from "./src/router"
+import fs from "fs"
+import { base64_img } from "./src/config"
 
 const app = new Koa()
 
-app.keys = [ SECRET_KEY ]
+// const list = fs.readdirSync(process.cwd() + '/cache_0/2022/2/8')
 
-render(app, {
-  root: path.join(__dirname, 'src/views'),
-  layout: false,
-  viewExt: 'ejs',
-  cache: false,
-  debug: process.env.APP_NODE_ENV === "dev"
-})
+// list.forEach((item) => {
+//   const data = fs.readFileSync(process.cwd() + '/cache_0/2022/2/8/' + item, "base64")
+//   fs.writeFileSync(`${process.cwd() + '/cache_0/2022/2/8/'}${item.substring(0, item.length - 6)}.png`, base64_img + data, "base64")
+//   fs.rmSync(process.cwd() + '/cache_0/2022/2/8/' + item)
+// })
 
-app
-  .use(logger())
-  .use(cors())
-  .use(session({
-    key: SESSION_KEY
-  }, app))
-  .use(koaBody())
-  .use(router.routes(), router.allowedMethods())
-  .use(koaStatic(path.join(__dirname, './src/static')))
+// app.keys = [ SECRET_KEY ]
+
+// render(app, {
+//   root: path.join(__dirname, 'src/views'),
+//   layout: false,
+//   viewExt: 'ejs',
+//   cache: false,
+//   debug: process.env.APP_NODE_ENV === "dev"
+// })
+
+// app
+//   .use(logger())
+//   .use(cors())
+//   .use(session({
+//     key: SESSION_KEY
+//   }, app))
+//   .use(koaBody())
+//   .use(router.routes(), router.allowedMethods())
+//   .use(koaStatic(path.join(__dirname, './src/static')))
 
 app.listen(4000, () => {
   console.log(process.env.APP_NODE_ENV)
