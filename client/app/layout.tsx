107dfbd23fcb618c8afeb971f170caedecfacfd0
system.wangyafei <3218566389@qq.com>
2022-06-11 15:36:49 +0800

    feat：客户端搭建

---
+++ client/app/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+interface Props {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html>
+      <body>
+        { children }
+      </body>
+    </html>
+  )
+}
