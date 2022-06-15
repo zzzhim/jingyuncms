b4c4641d391852492612de21a2cb94133a4db740
system.wangyafei <3218566389@qq.com>
2022-06-15 19:42:26 +0800

    feat：token

---
+++ src/controllers/commonVideo.js
@@ -0,0 +1,36 @@
+import { MacVodModel } from '../model/vod'
+import response from '../utils/response'
+import { logger } from '../utils/logger'
+
+/**
+ *
+ * @param {number} typeId 分类id
+ * @param {number} typeId1 一级分类id
+ * @param {string} typeName 分类名
+ * @param {string} vodName 视频名
+ * @param {string} vodName 视频名
+ * @param {number} vodYear 年代
+ * @param {number} pageSize 页数
+ * @description 查询视频
+ */
+export const videoList = async ({ pageNo = 1, pageSize = 10, ...obj }) => {
+  try {
+    const { count = 0, rows = [] } = await MacVodModel.findAndCountAll({
+      where: {},
+      limit: parseInt(pageSize),
+      offset: parseInt(pageSize * (pageNo - 1)),
+    })
+
+    return response.success(
+      200,
+      {
+        total: count,
+        list: rows,
+      }
+    )
+  } catch (error) {
+    console.log(error)
+    logger.error(error)
+    return response.error(500)
+  }
+}
---
+++ src/utils/token.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+import { SECRET_KEY } from "../config"
+
+/**
+ * 
+ * @param {any} data
+ * 创建token
+ */
+export async function createToken(data) {
+  return jwt.sign(data, SECRET_KEY, { expiresIn: (24 * 31) + 'h' })
+}
+
+
+/**
+ * 
+ * @param {any} token
+ * 检验token
+ */
+export async function verifyToken(token) {
+  try {
+    jwt.verify(token, SECRET_KEY)
+  } catch (error) {
+    
+  }
+  return 
+}
