6ba55d61d267868c6780be100a700f816b1737a5
zzzhim <3218566389@qq.com>
2022-06-19 20:44:11 +0800

    feat：修改当前用户账户密码

--- server/src/controllers/cms/user.js
+++ server/src/controllers/cms/user.js
@@ -2,6 +2,7 @@
 import response from '../../utils/response'
 import { logger } from '../../utils/logger'
 import { UserModel } from '../../model/user'
+import md5 from 'md5'
 
 /**
  *
@@ -35,6 +36,67 @@ export const userList = async ({ pageNo = 1, pageSize = 10, ...obj }) => {
         list: rows,
       }
     )
+  } catch (error) {
+    logger.error(error)
+    return response.error(500, {}, error)
+  }
+}
+
+/**
+ * 
+ * @param {number} userId 用户ID
+ * @param {string} username 用户昵称
+ * @param {string} oldPassword 旧密码
+ * @param {string} newPassword 新密码
+ * @description 修改当前用户账户密码
+ */
+ export const userEditAccountAndPass = async ({ userId, username, oldPassword, newPassword }) => {
+  try {
+    const data = await UserModel.findOne({
+      where: {
+        id: userId,
+        password: md5(oldPassword),
+      }
+    })
+
+    if(!data) {
+      return response.warning(500, {}, "当前用户密码错误")
+    }
+
+    await data.update({
+      username: username,
+      password: md5(newPassword),
+    })
+
+    await data.save()
+
+    return response.success(200, {})
+  } catch (error) {
+    logger.error(error)
+    return response.error(500, {}, error)
+  }
+}
+
+/**
+ * 
+ * @param {number} userId 用户ID
+ * @param {string} role 权限 0 超级管理员 1 普通用户 2 管理员
+ * @description 修改用户权限
+ */
+ export const userRoleEdit = async ({ userId, role }) => {
+  try {
+    await UserModel.update(
+      {
+        role,
+      },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    )
+
+    return response.success(200, {})
   } catch (error) {
     logger.error(error)
     return response.error(500, {}, error)
